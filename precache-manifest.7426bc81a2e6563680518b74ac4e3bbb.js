593181294944859261931a391b7f2dce627fa58e
07aki <akshitverma@Akshits-MacBook-Pro.local>
2019-09-03 00:27:36 +0530

    Updates

---
+++ precache-manifest.7426bc81a2e6563680518b74ac4e3bbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5607365322c7adeed68350a5de4a1c2",
+    "url": "/monsters-rolodex-react-app/index.html"
+  },
+  {
+    "revision": "360873e6386a0c16e598",
+    "url": "/monsters-rolodex-react-app/static/css/main.daafab2c.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex-react-app/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "360873e6386a0c16e598",
+    "url": "/monsters-rolodex-react-app/static/js/main.3f399b84.chunk.js"
+  },
+  {
+    "revision": "b8ef1a2879c6ad662128",
+    "url": "/monsters-rolodex-react-app/static/js/runtime~main.10afe511.js"
+  }
+]);
